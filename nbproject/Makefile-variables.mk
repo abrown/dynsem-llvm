76af1731523b499bb3a5a4478badc7b771e155ff
Andrew Brown <andrew@casabrown.com>
2017-02-19 14:50:47 -0800

    Switch to Cygwin build chain (momentarily)

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=dynsem-llvm
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/dynsem-llvm
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/dynsem-llvm
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=dynsem-llvm.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/dynsem-llvm.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/dynsem-llvm.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
